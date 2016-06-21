191c0b4d42546bf755b6553b2fe79d384f992811
thatarvinguy <zadeharvin@gmail.com>
2016-06-21 02:24:34 -0700

    Add working map

--- MorMap/ViewController.swift
+++ MorMap/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(34.06, longitude: -118.41, zoom: 5);
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera);
+        mapView.myLocationEnabled = true;
+        self.view = mapView;
+        
+        let marker = GMSMarker();
+        marker.position = CLLocationCoordinate2DMake(34.06, -118.41);
+        marker.title = "Team 1515";
+        marker.snippet = "MorTorq";
+        marker.map = mapView;
     }
 
     override func didReceiveMemoryWarning() {
