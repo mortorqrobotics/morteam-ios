b49b5514c046c87a2b40d9197001ff31337e8c29
thatarvinguy <zadeharvin@gmail.com>
2016-09-25 14:31:54 -0700

    Add announcement cell

---
+++ MorTeam/AnnouncementCell.swift
@@ -0,0 +1,17 @@
+//
+//  AnnouncementCell.swift
+//  MorTeam
+//
+//  Created by arvin zadeh on 9/25/16.
+//  Copyright © 2016 MorTorq. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AnnouncementCell: UICollectionViewCell {
+    
+    @IBOutlet var profilePic: UIImageView!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var content: UITextView!
+}
